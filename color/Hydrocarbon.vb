b5830fb9d3e4c047cd46d921bdb6057da5ae3978
unknownsaying <15368104568@189.cn>
2025-06-04 16:13:40 +0800

    Update Hydrocarbon.vb

--- color/Hydrocarbon.vb
+++ color/Hydrocarbon.vb
@@ -9,7 +9,7 @@ Class ReadOnly IC1H4 implements CH4,C5H8,C13H24
     End Class
 Class ReadOnly IC2H2 implements C2H2,C4H2,C6H2
     End Class
-Class ReadOnly IC2h4 implements C2H4,C6H8,C14H16
+Class ReadOnly IC2H4 implements C2H4,C6H8,C14H16
     End Class
 Console.WriteLine(c)
 
