38aaec52258694bc370b6a38613572ad07421376
unknownsaying <15368104568@189.cn>
2025-06-04 16:09:40 +0800

    Update SUM.vb

--- guess/SUM.vb
+++ guess/SUM.vb
@@ -1,6 +1,4 @@
-Imports det1234.vb
-Imports det5678.vb
-Imports det9101112.vb
+Imports det.vb
 Imports one.vb
 Imports oppoone.vb
 Imports pyramid.vb
@@ -10,7 +8,7 @@ Imports antipyramid.vb
 Imports tri.vb
 Imports antitri.vb
 
-const DET = det1234 + det5678 + det9101112
+const DET = det
 const tri = tri - antitri
 const ONE = one + oppoone
 const Square = square * antisquare
