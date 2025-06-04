c61ae200ea07e9a02bf32a02f95ebb227758290d
unknownsaying <15368104568@189.cn>
2025-06-04 16:05:05 +0800

    Update fermat.vb

--- vb/fermat.vb
+++ vb/fermat.vb
@@ -1,3 +1,7 @@
+Imports "XYZ.vb"
+Imports "clock.vb"
+Imports "null.vb"
+Imports "zero.vb"
 X =
 BigXtensor + midxtensor + smallxtensor
 Y =
@@ -8,3 +12,7 @@ X ^ 2 + Y ^ 2 = Z ^ 2
 zerotensor = onetensor + ninetensor
 innertensor + outertensor = antiinouttensor + antioutintensor = inouttensor + outintensor
 Nulltensor = Ntensor + NNtensor + NNNtensor
+Console.WriteLine(BigXtensor, midxtensor, smallxtensor, BigYtensor, midytensor, smallytensor, BigZtensor, midztensor, smallztensor)
+Console.WriteLine(zerotensor, onetensor, ninetensor)
+Console.WriteLine(innertensor, outertensor, inouttensor, outintensor, antiinouttensor, antioutintensor)
+Console.WriteLine(Nulltensor, Ntensor, NNtensor, NNNtensor)
